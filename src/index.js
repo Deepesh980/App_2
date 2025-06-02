5ad62b588da7478ec6f1b126af236308e01506c4
Deepesh980 <deepeshmalik.me@gmail.com>
2025-06-02 12:51:19 +0530

    User and Video Model Created

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ dotenv.config({
     path: './.env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 4000 , () => {
+        console.log(`Server is running on port ${process.env.PORT || 4000}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB Connection Failed",error)
+})
